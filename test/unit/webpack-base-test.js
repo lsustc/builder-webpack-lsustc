b44b0311fa196fb450f4fcb057a1bed1fe5d9a64
lsustc <lsustc@mail.ustc.edu.cn>
2021-10-14 20:04:13 +0800

    test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -5,7 +5,7 @@ describe('wenpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base');
     console.log(baseConfig);
     it('entry', () => {
-        assert.equal(baseConfig.entry.index.includes('builder-webpack-lsustc/test/smoke/template/src/index/index.js'), true);
-        assert.equal(baseConfig.entry.search.includes('builder-webpack-lsusct/test/smoke/template/src/search/index.js'), true);
+        assert.equal(baseConfig.entry.index.indexOf('builder-webpack-lsustc/test/smoke/template/src/index/index.js') > -1, true);
+        assert.equal(baseConfig.entry.search.indexOf('builder-webpack-lsusct/test/smoke/template/src/search/index.js') > -1, true);
     });
 });
